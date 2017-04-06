5b9e539810eaeaf8f547afefa65244b7d11d2133
Ciprian Dorin Craciun <ciprian@volution.ro>
2017-04-07 01:42:33 +0300

    Add `PasswordCommand` option which overrides `SSH_ASKPASS` and should always provide a password  (this is used only for login passwords)

--- readconf.h
+++ readconf.h
@@ -165,6 +165,8 @@ typedef struct {
 	int	jump_port;
 	char   *jump_extra;
 
+	char   *password_command;
+
 	char	*ignored_unknown; /* Pattern list of unknown tokens to ignore */
 }       Options;
 
